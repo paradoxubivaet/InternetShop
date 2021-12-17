bf0bc80c34c8e8e384a71a6a9466ff7d305ba41a
BlackBroke <paradox.ubivaet@mail.ru>
2021-12-18 05:09:54 +0800

    Добавить реализацию репозитория книг в памяти

--- domain/InternetShop/Book.cs
+++ domain/InternetShop/Book.cs
@@ -2,7 +2,16 @@
 
 namespace InternetShop
 {
-    public class Class1
+    public class Book
     {
+        public int Id { get; }
+        public string Title { get; }
+
+        public Book(int id,string title)
+        {
+            Id = id;
+            Title = title;
+
+        }
     }
 }
