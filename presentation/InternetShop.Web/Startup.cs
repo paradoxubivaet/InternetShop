a413373308194c0f8724e89701260273b14eb1ad
BlackBroke <paradox.ubivaet@mail.ru>
2021-12-18 23:05:38 +0800

    Создать страницу поиска

--- presentation/InternetShop.Web/Startup.cs
+++ presentation/InternetShop.Web/Startup.cs
@@ -1,3 +1,4 @@
+using InternetShop.Memory;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace InternetShop.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<IBookRepository, BookRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
