321ca1b319ff46e4c9d8326463a7279a5e8de24d
BlackBroke <paradox.ubivaet@mail.ru>
2021-12-19 22:04:42 +0800

    Реализовать поиск по ISBN и автору

--- domain/InternetShop/Book.cs
+++ domain/InternetShop/Book.cs
@@ -1,17 +1,33 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace InternetShop
 {
     public class Book
     {
         public int Id { get; }
+        public string Isbn { get; } 
+        public string Author { get; }
         public string Title { get; }
 
-        public Book(int id,string title)
+        public Book(int id,string isbn, string author, string title)
         {
             Id = id;
+            Isbn = isbn;
+            Author = author;
             Title = title;
+        }
 
+        internal static bool IsIsbn(string s)
+        {
+            if(s == null)
+                return false;
+
+            s = s.Replace("-", "")
+                 .Replace(" ", "")
+                 .ToUpper();
+
+            return Regex.IsMatch(s, @"^ISBN\d{10}(\d{3})?$");
         }
     }
 }
