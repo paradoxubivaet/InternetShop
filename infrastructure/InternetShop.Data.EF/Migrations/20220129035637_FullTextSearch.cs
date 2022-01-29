7192b0c46166a7d1e62c24cc8ea3ba5b20ca66bd
BlackBroke <paradox.ubivaet@mail.ru>
2022-01-29 12:05:14 +0800

    Добавить полнотекстовый поиск

---
+++ infrastructure/InternetShop.Data.EF/Migrations/20220129035637_FullTextSearch.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InternetShop.Data.EF.Migrations
+{
+    public partial class FullTextSearch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE FULLTEXT CATALOG InternetShopFullTextCatalog AS DEFAULT", suppressTransaction: true);
+            migrationBuilder.Sql("CREATE FULLTEXT INDEX ON BOOKS(Author, Title) KEY INDEX PK_Books WITH STOPLIST = SYSTEM", suppressTransaction: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FULLTEXT INDEX ON Books", suppressTransaction: true);
+            migrationBuilder.Sql("DROP FULLTEXT CATALOG InternetShopFullTextCatalog", suppressTransaction: true);
+        }
+    }
+}
